370042ff9ea4da63ca06d199148ec77491711900
kevocodes <00116120@uca.edu.sv>
2025-09-01 16:20:47 -0600

    :sparkles: feat: update homescreen design

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="w-full min-h-[100dvh] flex items-center justify-center p-4 flex-col">
-      <section className="p-8 flex-col justify-center items-center text-center space-y-4 max-w-3xl">
+      <section className="p-8 flex w-full flex-col justify-center items-center text-center space-y-4 max-w-2xl">
         
         <h1 className="text-4xl font-bold tracking-tighter md:text-5xl lg:text-7xl">
           Roots <AuroraText>Tasks</AuroraText>
@@ -14,7 +14,7 @@ export default function Home() {
           Task management system for technical assessment.
         </p>
 
-        <section className="flex gap-4 sm:flex-row flex-col">
+        <section className="flex gap-4 sm:flex-row flex-col w-full">
           <Button className="flex-1">
             <Link href="/tasks">View Tasks</Link>
           </Button>
